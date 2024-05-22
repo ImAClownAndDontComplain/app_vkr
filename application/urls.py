d337fcd574a702e5b6478b2d591a1cb5834b29bc
ImAClownAndDontComplain <94308517+ImAClownAndDontComplain@users.noreply.github.com>
2024-05-22 20:50:19 +0500

    hz

--- application/urls.py
+++ application/urls.py
@@ -3,7 +3,7 @@ from . import views
 from .serializers import ToAnalyzeSerializer
 urlpatterns = [
   # path('home/', views.home, name='home_path'),
-  # path('hz/', views.get_username, name='hz'),
+  path('hz/', views.get_username, name='hz'),
   path('all_ingr_names/', views.get_all_ingr_names, name='all_ingr_names'),
   path('analyze/', views.Analyze.as_view(), name='analyze'),
   path('analyze/get_analysis/<int:record_id>', views.GetAnalysis.as_view(), name='get_analysis'),
